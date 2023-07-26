f80a1a6b787c63c36d313e0a6bbfe55458ff3f6b
Liam Collod <lcollod@gmail.com>
2023-07-26 14:14:18 +0200

    [dev] fixed launcher script
    PYTHON env var not build properly, prevent streamlit to auto-update

--- dev/run-streamlit.sh
+++ dev/run-streamlit.sh
@@ -1,2 +1,3 @@
-export PYTHONPATH=$PYTHONPATH:$CWD/streamlit_colourplotting
+export PYTHONPATH=$PWD/streamlit_colourplotting:$PWD/streamlit_colourplotting/ui:$PWD/streamlit_colourplotting/core
+echo $PYTHONPATH
 python -m streamlit run src/app.py
