9eb89df78843e5e6cd083a7c84d01d2241cea33c
Liam Collod <lcollod@gmail.com>
2023-07-28 22:19:14 +0200

    fix USER_SOURCE_TYPE widget setting again

--- streamlit_colourplotting/ui/_sidebar.py
+++ streamlit_colourplotting/ui/_sidebar.py
@@ -6,18 +6,22 @@ from streamlit_colourplotting.ui import config
 
 
 @widgetify
-def widget_source_type(key):
+def widget_source_type(key, force_update=False):
+    if key not in streamlit.session_state or force_update:
+        streamlit.session_state[key] = config().USER_SOURCE_TYPE.value
+        return
+
     config().USER_SOURCE_TYPE = SourceType(streamlit.session_state[key])
 
 
 def create_sidebar():
     streamlit.title("Options".upper())
 
+    widget_source_type(force_update=True)
     options = SourceType.labels()
     streamlit.selectbox(
         label="Source",
         options=options,
-        index=options.index(config().USER_SOURCE_TYPE.value),
         help="Choose which data type you want to plot.",
         key=str(widget_source_type),
         on_change=widget_source_type,
