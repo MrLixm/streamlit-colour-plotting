c8e183fcd6452ff9dd80035cf84723380b111b12
Liam Collod <lcollod@gmail.com>
2023-08-04 20:01:21 +0200

    launcher: add headless option
    prevent opening a new browser window each time

--- dev/run-streamlit.sh
+++ dev/run-streamlit.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PWD/streamlit_colourplotting:$PWD/streamlit_colourplotting/ui:$PWD/streamlit_colourplotting/core
 echo $PYTHONPATH
-python -m streamlit run src/app.py
+python -m streamlit run src/app.py --server.headless true
