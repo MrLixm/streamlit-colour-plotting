1adecf86851db275061685b79e6e9c029c3690e7
Liam Collod <lcollod@gmail.com>
2026-03-19 12:05:04 +0100

    chore: remove unused script

--- dev/run-streamlit.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=$PWD/streamlit_colourplotting:$PWD/streamlit_colourplotting/ui:$PWD/streamlit_colourplotting/core
-# python logging level expected
-export STCP_APP_LOG_LEVEL=DEBUG
-echo $PYTHONPATH
-python -m streamlit run src/app.py --server.headless true
